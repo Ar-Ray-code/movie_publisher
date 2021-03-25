0a9b7ce0ec69517786b81800b61e859d328ca7d3
Ar-Ray-code <ray255ar@gmail.com>
2021-03-26 08:55:58 +0900

    foxy

--- src/movie_publisher.cpp
+++ src/movie_publisher.cpp
@@ -1,38 +1,66 @@
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
-int main(int argc, char** argv) {
-	ros::init (argc, argv, "camera");
-	ros::NodeHandle nh("~"); 
+rclcpp::Clock system_clock(RCL_ROS_TIME);
 
-	std::string name;
-	nh.getParam("video_name",name);
+class movie_pub : public rclcpp::Node
+{
+	public:
+		std::shared_ptr<rclcpp::Node> param_get_node;
+		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub;
 
-	cv::Mat image;
-	cv::VideoCapture camera(name.c_str());
-	float flame_rate = camera.get(cv::CAP_PROP_FPS);
+		sensor_msgs::msg::Image::SharedPtr msg;
 
-	if (!camera.isOpened()) {
-		ROS_INFO("failed to open camera.");
-		return -1;
-	}
-	ros::Rate looprate (flame_rate); // capture image at 10Hz
-	std::cout << flame_rate << std::endl;
+		std_msgs::msg::Header header;
+		// std::shared_ptr<sensor_msgs::msg::Image> image_pub;
+
+		std::string name;
 
 
-	image_transport::ImageTransport it(nh);
-	image_transport::Publisher image_pub = it.advertise("color/image_raw", flame_rate);
+		movie_pub(const std::string &name_space, rclcpp::NodeOptions options):rclcpp::Node(name_space, options)
+		{
+
+			name = "/home/ubuntu/Desktop/video.mp4";
+
+			cv::Mat image;
+			cv::VideoCapture camera(name.c_str());
+			
+			float flame_rate = camera.get(cv::CAP_PROP_FPS);
+
+			if (!camera.isOpened()) {
+				std::cout << name << " failed to open camera." << std::endl;
+			}
+			rclcpp::Rate looprate (flame_rate); // capture image at 10Hz
+			std::cout << flame_rate << std::endl;
+
+			image_pub = this->create_publisher<sensor_msgs::msg::Image>("image_raw",1);
 	
-	while(ros::ok()) {
-		camera >> image;
-		sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-		image_pub.publish(msg);
-		ros::spinOnce();
-		looprate.sleep();
-	}
+			while(rclcpp::ok())
+			{
+				camera >> image;
+				cv::resize(image, image,cv::Size(416, 234));
+				
+				header.stamp = system_clock.now();
+				header.frame_id = "/map";
+
+				msg = cv_bridge::CvImage(header, "bgr8", image).toImageMsg();
+				image_pub->publish(*msg);
+				looprate.sleep();
+			}
+		}
+	
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+	rclcpp::NodeOptions options;
+	auto node = std::make_shared<movie_pub>("movie_pub", options);
+	
+	rclcpp::spin(node);
+	rclcpp::shutdown();
 	return 0;
 }
- 
