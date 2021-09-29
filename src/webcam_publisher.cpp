962371b2ee673f5596f5aca3f2335a0253d114c3
Ar-Ray-code <ray255ar@gmail.com>
2021-09-29 23:21:36 +0900

    fix webcam_pub

--- src/webcam_publisher.cpp
+++ src/webcam_publisher.cpp
@@ -14,13 +14,17 @@ class movie_pub : public rclcpp::Node
 		sensor_msgs::msg::Image::SharedPtr msg;
 		std_msgs::msg::Header header;
 
+		rclcpp::TimerBase::SharedPtr timer;
+
 		int camera_num;
 		int width;
 		int height;
+		cv::Mat image;
+
+		cv::VideoCapture camera;
 
 		movie_pub(const std::string &name_space, rclcpp::NodeOptions options):rclcpp::Node(name_space, options)
 		{
-			cv::Mat image;
 			// declare parameter
 			this->declare_parameter("camera_num", 0);
 			this->declare_parameter("width", 640);
@@ -31,29 +35,34 @@ class movie_pub : public rclcpp::Node
 			width = this->get_parameter("width").as_int();
 			height = this->get_parameter("height").as_int();
 	
-			cv::VideoCapture camera(camera_num);
+			// cv::VideoCapture camera(camera_num);
+			camera.open(camera_num);
 			
 			float flame_rate = camera.get(cv::CAP_PROP_FPS);
 
 			if (!camera.isOpened()) {
 				std::cout << camera_num << " failed to open camera." << std::endl;
 			}
-			rclcpp::Rate looprate (flame_rate);
+			// rclcpp::Rate looprate (flame_rate);
 			std::cout << flame_rate << std::endl;
 
 			image_pub = this->create_publisher<sensor_msgs::msg::Image>("image_raw",10);
-	
-			while(rclcpp::ok())
-			{
-				camera >> image;
-				cv::resize(image, image,cv::Size(width,height));
-				
-				header.stamp = system_clock.now();
-				header.frame_id = "/map";
 
-				msg = cv_bridge::CvImage(header, "bgr8", image).toImageMsg();
-				image_pub->publish(*msg);
-			}
+			std::chrono::nanoseconds duraction(int(1/float(flame_rate)*1000000000));
+
+            timer = this->create_wall_timer(duraction, std::bind(&movie_pub::pub_movie, this));
+		}
+
+		void pub_movie()
+		{
+			camera >> image;
+			cv::resize(image, image,cv::Size(width,height));
+				
+			header.stamp = system_clock.now();
+			header.frame_id = "/map";
+
+			msg = cv_bridge::CvImage(header, "bgr8", image).toImageMsg();
+			image_pub->publish(*msg);
 		}
 };
 
